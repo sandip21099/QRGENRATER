5ac8b34a693864405cb0184d3d9a3de52980c07b
sandip786 <sandeepyadavj7865@gmail.com>
2023-10-15 21:50:43 +0530

    first

---
+++ script.js
@@ -0,0 +1,14 @@
+let imgBox = document.getElementById("imgbox");
+let qrimage = document.getElementById("qrimage");
+let qrtext = document.getElementById("qrtext");
+
+function generate(){
+  if(qrtext.value.length > 0){
+    qrimage.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrtext.value;
+    imgBox.classList.add("show-img")
+  }
+  else{
+    qrtext.classList.add("error")
+  }
+  
+}
